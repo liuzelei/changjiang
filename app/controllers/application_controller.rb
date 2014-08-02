a6ac2ead1876ad935fc88594c1ffd036f62cbb4c
liuzelei <liuzelei@gmail.com>
2014-08-03 01:48:20 +0800

    set permit function before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :match_permit
   layout :layout_by_resource
+
+  private
+  def match_permit
+    resource = controller_name.singularize.to_sym
+    action = action_name.singularize.to_sym
+    method_each_action = "#{action}_#{resource}_params"
+    method_all_in_one = "#{resource}_params"
+    params[resource] &&= (send(method_each_action) if respond_to?(method_each_action, true)) || (send(method_all_in_one) if respond_to?(method_all_in_one, true))
+  end
 
   def layout_by_resource
     if devise_controller?
