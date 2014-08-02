0c15139ed8be373d7f27b193db95dfd648565d70
liuzelei <liuzelei@gmail.com>
2014-08-03 00:45:12 +0800

    first init

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :cars
 
   devise_for :users
+
+  root 'orders#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
